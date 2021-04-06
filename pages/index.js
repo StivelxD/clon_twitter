14240d02a72b3fbb6ec40c4b42e1a6d67644c365
St1v3l <david-cardona-17@hotmail.com>
2021-04-06 17:40:38 -0500

    primeros trazos

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+const Index = () => {
+    return(
+        <div className="container mx-auto">
+            <div className="w-full bg-green-600 text-center text-4xl">
+                <h1>Hello World !</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Index;
